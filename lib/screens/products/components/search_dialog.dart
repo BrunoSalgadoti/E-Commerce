fb7624b32b37c3ac413a10564840d68c85c61bdc
Bruno <brunosalgadoti@gmail.com>
2023-02-20 17:19:05 -0300

    Criando: Pesquisa de Produtos

---
+++ lib/screens/products/components/search_dialog.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SearchDialog extends StatelessWidget {
+  const SearchDialog({Key? key , required this.initialText}) : super(key: key);
+
+  final String initialText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+            top: 2,
+            left: 5,
+            right: 5,
+            child: Card(
+                child: TextFormField(
+                  initialValue: initialText,
+                  textInputAction: TextInputAction.search,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    hintText: 'Pesquise o produto desejado',
+                  border: InputBorder.none,
+                  contentPadding: const EdgeInsets.symmetric(vertical: 15),
+                  prefixIcon: IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    color: Colors.grey[700],
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  )
+                  ),
+                  onFieldSubmitted: (text) {
+                    Navigator.of(context).pop(text);
+                  },
+            )
+            )
+        )
+      ],
+    );
+  }
+}
